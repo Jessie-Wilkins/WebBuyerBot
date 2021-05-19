8815581db8374bd89483f9ff39cf765d59345c34
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2021-05-19 08:58:35 -0500

    Added failing test for website retrieval

--- WebBuyerTest.py
+++ WebBuyerTest.py
@@ -3,8 +3,9 @@ import WebBuyer
 
 class WebBuyerTest(unittest.TestCase):
 
-    def test_one_equal_one(self):
-        self.assertEqual(1, 1)
+    def test_if_website_can_be_loaded(self):
+        WebBuyer.load_website("https://www.walmart.com/ip/Dummy-Stress-Test-Video-Game-Hardware-19/813808061")
+        self.assertEqual("https://www.walmart.com/ip/Dummy-Stress-Test-Video-Game-Hardware-19/813808061", WebBuyer.get_website())
 
 if __name__ == '__main__':
     unittest.main()
