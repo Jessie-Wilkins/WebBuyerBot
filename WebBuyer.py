366f0ea150031f0abc7467e927660ca92b726a7b
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2021-05-24 16:31:51 -0500

    Added failing test

--- WebBuyer.py
+++ WebBuyer.py
@@ -10,3 +10,20 @@ class WebBuyer():
     
     def get_website():
         return WebBuyer.website
+    
+    def ping_website():
+        chrome_options = webdriver.ChromeOptions()
+
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+
+        driver.get(WebBuyer.get_website())
+
+        if driver.current_url == WebBuyer.get_website():
+            return "accessible"
+
+        else:
+            return "inaccessible"
--- WebBuyerTest.py
+++ WebBuyerTest.py
@@ -7,5 +7,10 @@ class WebBuyerTest(unittest.TestCase):
         WebBuyer.load_website("https://www.walmart.com/ip/Dummy-Stress-Test-Video-Game-Hardware-19/813808061")
         self.assertEqual("https://www.walmart.com/ip/Dummy-Stress-Test-Video-Game-Hardware-19/813808061", WebBuyer.get_website())
 
+    def test_if_website_can_be_pinged(self):
+        WebBuyer.load_website("https://www.walmart.com/ip/Dummy-Stress-Test-Video-Game-Hardware-19/813808061")
+        ping_status = WebBuyer.ping_website()
+        self.assertEqual("accessible", ping_status)
+
 if __name__ == '__main__':
     unittest.main()
